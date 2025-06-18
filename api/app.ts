9b9b21eb2bf284e6654d34b65dd6240f0fb847a4
Nikoolaii <nikolailemerre@gmail.com>
2025-06-18 11:02:35 +0200

    ✨ Add Order routes and product data files, update index.html title

--- api/app.ts
+++ api/app.ts
@@ -1,16 +1,25 @@
-import express, { Express, Request, Response , Application } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response , Application } from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
 
-//For env File 
-dotenv.config();
+import OrderRoutes from './routes/Order.route';
+
+dotenv.config()
 
 const app: Application = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to Express & TypeScript Server');
+  res.send('Welcome to Express & TypeScript Server')
 });
 
+app.use('/order', OrderRoutes)
+
 app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+  console.log(`Server is Fire at http://localhost:${port}`)
 });
