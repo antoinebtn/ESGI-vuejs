3ca500f52f0a4d7e1c60cc1f5eded8dcd37e1910
antoinebtn <antoinebtn@gmail.com>
2025-06-18 10:54:27 +0200

    Home View

--- app/src/Views/HomeView.vue
+++ app/src/Views/HomeView.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-  <main class="container mx-auto px-4 py-8 pt-24"> <!-- Ajout de pt-24 pour compenser la hauteur de la navbar -->
+  <main class="container mx-auto px-4 py-8 pt-24">
     <section class="mb-12">
       <h1 class="text-4xl font-bold text-center mb-6">Bienvenue chez Burger Express</h1>
       <p class="text-xl text-center text-gray-600 mb-8">Les meilleurs burgers de la ville, livrés chez vous</p>
