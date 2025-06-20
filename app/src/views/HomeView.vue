91bc23d0f09710c2d7b9560a5d308bbd0d1a34aa
Nikoolaii <nikolailemerre@gmail.com>
2025-06-20 14:29:35 +0200

    💄 Update layout for responsive design in HomeView

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -19,7 +19,7 @@ const handleLogoClick = () => {
     </section>
 
     <div class="flex flex-col md:flex-row items-center justify-between bg-white shadow-lg rounded-lg p-8 mb-12">
-      <div class="w-1/2 flex flex-col justify-center pr-8">
+      <div class="lg:w-1/2 flex flex-col justify-center pr-8">
         <p class="text-2xl font-semibold text-green-500 mb-4">Bun Appetit</p>
         <p class="text-lg text-gray-700 mb-6">
           Découvrez nos burgers faits maison, préparés avec des ingrédients frais et de qualité. Que vous soyez fan de
